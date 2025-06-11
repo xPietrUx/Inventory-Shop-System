530efae8da1314486d21c2288f4b2dbbb8397e6f
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-06-11 16:50:11 +0200

    Dodanie Roles

--- users/models.py
+++ users/models.py
@@ -1,14 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
 
-class Users(models.Model):
+class Roles(models.Model):
+    role_name = models.CharField()
+    description = models.CharField()
+
+
+class Users(AbstractUser):
     name = models.CharField()
     surname = models.CharField()
     email = models.EmailField(unique=True)
-    positon = models.CharField()
+    position = models.CharField()
     id_role = models.ForeignKey("Role", on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.Name} {self.Surname} na stanowisku: {self.Positon}"
+        return f"{self.Name} {self.Surname} na stanowisku: {self.position}"
