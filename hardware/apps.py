90cecb2f96a779c0970607bfc53b9f0daf89cf9e
Dominik <101473446+b14ucky@users.noreply.github.com>
2025-06-14 00:21:11 +0200

    fix: change app name from software to hardware

--- hardware/apps.py
+++ hardware/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SoftwareConfig(AppConfig):
+class HardwareConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "software"
+    name = "hardware"
