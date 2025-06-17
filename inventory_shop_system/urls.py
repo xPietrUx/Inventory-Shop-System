9b5312d3cfa23769b6bdb3e6fa377971a620ac50
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-06-17 15:36:06 +0200

    feat: dodanie konfiguracji adresu URL multimediów do urlpatterns

--- inventory_shop_system/urls.py
+++ inventory_shop_system/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("licenses/", include("licenses.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
