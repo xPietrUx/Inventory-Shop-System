9b7e7f0a064ad613dd98cd956d950d03072a6b73
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-06-17 16:14:45 +0200

    fix: poprawa ścieżek URL dla edycji i usuwania modeli oprogramowania i licencji

--- licenses/urls.py
+++ licenses/urls.py
@@ -8,11 +8,13 @@ urlpatterns = [
     # URLs for Software model
     path("software/", views.software_list_view, name="software_list"),
     path("software/add/", views.software_add_view, name="software_add"),
-    path("software/edit/", views.software_edit_view, name="software_edit"),
-    path("software/delete/", views.software_delete_view, name="software_delete"),
+    path("software/<int:pk>/edit/", views.software_edit_view, name="software_edit"),
+    path(
+        "software/<int:pk>/delete/", views.software_delete_view, name="software_delete"
+    ),
     # URLs for License model
     path("license/", views.license_list_view, name="license_list"),
     path("license/add/", views.license_add_view, name="license_add"),
-    path("license/edit/", views.license_edit_view, name="license_edit"),
-    path("license/delete/", views.license_delete_view, name="license_delete"),
+    path("license/<int:pk>/edit/", views.license_edit_view, name="license_edit"),
+    path("license/<int:pk>/delete/", views.license_delete_view, name="license_delete"),
 ]
