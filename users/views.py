2e18b7758ccf80db18e3527be1327948d60360eb
Dominik <101473446+b14ucky@users.noreply.github.com>
2025-06-20 00:01:43 +0200

    feat: add user management views and forms for adding users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Users
+from django import forms
 
-# Create your views here.
+
+class UsersForm(forms.ModelForm):
+    class Meta:
+        model = Users
+        fields = [
+            "name",
+            "surname",
+            "position",
+            "id_role",
+            "email",
+            "username",
+            "password",
+        ]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"placeholder": "First Name"},
+            ),
+            "surname": forms.TextInput(
+                attrs={"placeholder": "Last Name"},
+            ),
+            "position": forms.TextInput(
+                attrs={"placeholder": "Position"},
+            ),
+            "email": forms.EmailInput(
+                attrs={"placeholder": "E-mail"},
+            ),
+            "username": forms.TextInput(
+                attrs={"placeholder": "Username"},
+            ),
+            "password": forms.PasswordInput(
+                attrs={"placeholder": "Password"},
+            ),
+        }
+
+
+def users_list_view(request):
+    users = Users.objects.all()
+    active_nav = "users"
+    active_page_title = "Users"
+
+    return render(
+        request,
+        "users/users_list.html",
+        {
+            "users": users,
+            "active_nav": active_nav,
+            "active_page_title": active_page_title,
+        },
+    )
+
+
+def users_add_view(request):
+    active_nav = "users"
+    active_page_title = "Users"
+
+    if request.method == "POST":
+        form = UsersForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("users:users_list")
+    else:
+        form = UsersForm()
+
+    return render(
+        request,
+        "users/users_add.html",
+        {
+            "form": form,
+            "active_nav": active_nav,
+            "active_page_title": active_page_title,
+        },
+    )
