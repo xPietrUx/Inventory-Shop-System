3bed49d3f6c83ef24838f5d35ee4ecf7a36fa737
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-06-20 20:23:39 +0200

    Refactor

--- hardware/urls.py
+++ hardware/urls.py
@@ -1,49 +1,13 @@
 from django.urls import path
-
-from .views import (
-    HardwareListView,
-    HardwareAddView,
-    HardwareEditView,
-    HardwareDeleteView,
-    HardwareHistoryListView,
-    HardwareHistoryEditView,
-    HardwareHistoryDeleteView,
-    ProjectListView,
-    ProjectAddView,
-    ProjectEditView,
-    ProjectDeleteView,
-)
+from . import views
 
 app_name = "hardware"
+
 urlpatterns = [
-    # Hardware
-    path("hardware/", HardwareListView.as_view(), name="hardware_list"),
-    path("hardware/add/", HardwareAddView.as_view(), name="hardware_add"),
-    path("hardware/<int:pk>/edit/", HardwareEditView.as_view(), name="hardware_edit"),
-    path(
-        "hardware/<int:pk>/delete/",
-        HardwareDeleteView.as_view(),
-        name="hardware_delete",
-    ),
-    # Hardware History
-    path(
-        "hardware/history/<int:hardware_id>/",
-        HardwareHistoryListView.as_view(),
-        name="hardware_history",
-    ),
-    path(
-        "hardware/history/edit/<int:pk>/",
-        HardwareHistoryEditView.as_view(),
-        name="hardware_history_edit",
-    ),
-    path(
-        "hardware/history/delete/<int:pk>/",
-        HardwareHistoryDeleteView.as_view(),
-        name="hardware_history_delete",
-    ),
-    # Projects
-    path("projects/", ProjectListView.as_view(), name="project_list"),
-    path("projects/add/", ProjectAddView.as_view(), name="project_add"),
-    path("projects/edit/<int:pk>/", ProjectEditView.as_view(), name="project_edit"),
-    path("projects/delete/<int:pk>/", ProjectDeleteView.as_view(), name="project_delete"),
+    # Hardware paths
+    path("hardware/", views.hardware_list_view, name="hardware_list"),
+    # History paths
+    path("history/", views.hardware_history_list_view, name="hardware_history_list"),
+    # Project paths
+    path("project/", views.project_list_view, name="project_list"),
 ]
