eb76a630dd54b652e0aaa42790519fe25eaa47d9
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-06-20 22:15:22 +0200

    style: reorganize urlpatterns for improved readability and consistency

--- hardware/urls.py
+++ hardware/urls.py
@@ -4,10 +4,18 @@ from . import views
 app_name = "hardware"
 
 urlpatterns = [
-    # Hardware paths
+    # Hardware
     path("hardware/", views.hardware_list_view, name="hardware_list"),
-    # History paths
-    path("history/", views.hardware_history_list_view, name="hardware_history_list"),
-    # Project paths
+    path("hardware/add/", views.hardware_add_view, name="hardware_add"),
+    path("hardware/<int:pk>/edit/", views.hardware_edit_view, name="hardware_edit"),
+    path(
+        "hardware/<int:pk>/delete/", views.hardware_delete_view, name="hardware_delete"
+    ),
+    # History
+    path("history/", views.history_list_view, name="history_list"),
+    # Projects
     path("project/", views.project_list_view, name="project_list"),
+    path("project/add/", views.project_add_view, name="project_add"),
+    path("project/<int:pk>/edit/", views.project_edit_view, name="project_edit"),
+    path("project/<int:pk>/delete/", views.project_delete_view, name="project_delete"),
 ]
