a566f4b4f879eea6ceeb79ca932e20a9f7585be7
Piotr Dusiński <134306002+xPietrUx@users.noreply.github.com>
2025-06-22 15:20:35 +0200

    Usinięcie operacji w Roles (#32)
    * fix(roles): remove 'Add Roles' link and action buttons from roles list
    
    * fix(roles): remove unused role management paths from URLs
    
    * fix(roles): remove unused role management views and associated logic

--- users/urls.py
+++ users/urls.py
@@ -12,9 +12,6 @@ urlpatterns = [
     path("users/<int:pk>/delete/", views.users_delete_view, name="users_delete"),
     # roles paths
     path("roles/", views.roles_list_view, name="roles_list"),
-    path("roles/add/", views.roles_add_view, name="roles_add"),
-    path("roles/<int:pk>/edit/", views.roles_edit_view, name="roles_edit"),
-    path("roles/<int:pk>/delete/", views.roles_delete_view, name="roles_delete"),
     # redirect from / to users/
     path("", RedirectView.as_view(url="users/", permanent=False)),
     # auth paths
